3a2bafd1e1f7c912b671464bc1228de2cae916a1
Tom Andrews <git@tomandrews.co.uk>
2014-08-14 18:48:34 +0300

    Admin get request is logging

--- incominghandler/incominghandler.go
+++ incominghandler/incominghandler.go
@@ -2,14 +2,29 @@ package incominghandler
 
 import "net/http"
 import "log"
+import "github.com/gorilla/mux"
 
-type IncomingHandler struct{}
+type IncomingHandler struct {
+	router *mux.Router
+}
 
 func (h IncomingHandler) Run() {
 	log.Print("Starting server on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", h))
+	h.router = h.createRouter()
+	log.Fatal(http.ListenAndServe(":8080", h.router))
 }
 
-func (h IncomingHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h IncomingHandler) createRouter() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("", h.FormPostHandler).Methods("POST")
+	router.HandleFunc("/admin", h.AdminGetHandler).Methods("GET")
+	return router
+}
+
+func (h IncomingHandler) FormPostHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("User Agent: %s | URI: %s", r.UserAgent(), r.RequestURI)
+}
+
+func (h IncomingHandler) AdminGetHandler(w http.ResponseWriter, r *http.Request) {
+	log.Print("Hello world")
 }
