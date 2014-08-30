d29179309763ce3b6f7f2b5265ceb39d16052820
Tom Andrews <git@tomandrews.co.uk>
2014-08-30 19:12:50 +0300

    Create subscriber struct to hold the data

---
+++ subscriber/subscriber.go
@@ -0,0 +1,21 @@
+package subscriber
+
+import "time"
+
+// A Subscriber of the list
+type Subscriber struct {
+	// The time they subscribed
+	SubscribeTime time.Time
+
+	// The user agent of the browser requested
+	UserAgent string
+
+	// The HTTP referer
+	Referer string
+
+	// The IP address the email was registered with
+	IPAddress string
+
+	// The email address
+	EmailAddress string
+}
