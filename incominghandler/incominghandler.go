e14321784f2bb420c85c14f362301a121966e340
Tom Andrews <git@tomandrews.co.uk>
2014-08-30 21:16:40 +0300

    Correct warning messages

--- incominghandler/incominghandler.go
+++ incominghandler/incominghandler.go
@@ -6,10 +6,13 @@ import "time"
 import "github.com/gorilla/mux"
 import s "github.com/tgandrews/mailbag/subscriber"
 
+// An IncomingHandler handles the incoming HTTP requests and performs appropriate
+// actions. The top leve controller if you will.
 type IncomingHandler struct {
 	router *mux.Router
 }
 
+// Run starts the server
 func (h IncomingHandler) Run() {
 	log.Print("Starting server on port 8080")
 	h.router = h.createRouter()
@@ -18,8 +21,8 @@ func (h IncomingHandler) Run() {
 
 func (h IncomingHandler) createRouter() *mux.Router {
 	router := mux.NewRouter()
-	router.HandleFunc("/", h.FormPostHandler).Methods("POST")
-	router.HandleFunc("/admin", h.AdminGetHandler).Methods("GET")
+	router.HandleFunc("/", h.formPostHandler).Methods("POST")
+	router.HandleFunc("/admin", h.adminGetHandler).Methods("GET")
 	return router
 }
 
@@ -35,6 +38,6 @@ func (h IncomingHandler) formPostHandler(w http.ResponseWriter, r *http.Request)
 	log.Printf("Subscriber: %v", subscriber)
 }
 
-func (h IncomingHandler) AdminGetHandler(w http.ResponseWriter, r *http.Request) {
+func (h IncomingHandler) adminGetHandler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Hello world")
 }
